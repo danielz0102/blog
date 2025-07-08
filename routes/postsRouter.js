41489f2967f8ef43d3ef2c1f6704b4bbf0f39893
danielz0102 <danyzglez@hotmail.com>
2025-07-07 18:23:38 -0600

    feat(postsRouter): validate post id in get/posts/:id

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,7 +1,12 @@
 import { Router } from 'express'
 import { PostsController } from '#controllers/PostsController.js'
+import { validateRequest } from '#middlewares/validateRequest.js'
 
 export const postsRouter = Router()
 
 postsRouter.get('/', PostsController.getAll)
-postsRouter.get('/:id', PostsController.get)
+postsRouter.get(
+  '/:id',
+  validateRequest({ hasIdParam: true }),
+  PostsController.get,
+)
