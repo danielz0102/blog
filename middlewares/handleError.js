04986e149cc2a5bc37b95ea018191e7e5de4b963
danielz0102 <danyzglez@hotmail.com>
2025-07-08 15:40:26 -0600

    fix(handleError): show error when JSON is malformed

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -2,6 +2,10 @@
 export function handleError(err, req, res, next) {
   console.error(err)
 
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ error: 'Body is not valid JSON' })
+  }
+
   res.status(500).json({
     error: 'An unexpected error occurred',
   })
