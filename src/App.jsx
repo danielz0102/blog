9f35806c2baaf077fafbb57518e1c1425d8b505b
danielz0102 <danyzglez@hotmail.com>
2025-07-08 18:41:08 -0600

    feat: add hook to fetch posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { usePosts } from '@/hooks/usePosts'
+
 export function App() {
+  const { posts, loading, error } = usePosts()
+
   return (
     <>
       <h1>Hello world</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Something went wrong</p>}
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <code>{JSON.stringify(post)}</code>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
