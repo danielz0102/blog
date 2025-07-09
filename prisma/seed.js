f06564e7e17bf20019913af53f2c64402ea73bc1
danielz0102 <danyzglez@hotmail.com>
2025-07-09 14:43:30 -0600

    chore: add script to seed database with fake posts

---
+++ prisma/seed.js
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const data = Array.from({ length: 30 }).map(() => ({
+  title: faker.lorem.sentence(),
+  content: faker.lorem.paragraphs(3),
+}))
+
+async function main() {
+  await prisma.post.createMany({
+    data,
+  })
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(() => {
+    prisma.$disconnect()
+  })
