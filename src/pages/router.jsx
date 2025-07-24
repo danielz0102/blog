9fa7e89d340f5f6629f87fc3fd50b125984430ef
danielz0102 <danyzglez@hotmail.com>
2025-07-23 18:47:47 -0600

    style(router): update imports

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -3,13 +3,13 @@ import { createBrowserRouter } from 'react-router'
 import App from '@templates/App'
 import { Home } from './Home'
 import { NotFound } from './NotFound'
+import { AuthForm } from '@organisms/AuthForm'
 
 import { authAction } from '@/lib/actions/authAction'
-import { logoutAction } from '../lib/actions/logout'
+import { logoutAction } from '@/lib/actions/logout'
 
 import { postsLoader } from './Home/loader'
-import { userLoader } from '../lib/loaders/userLoader'
-import { AuthForm } from '@organisms/AuthForm'
+import { userLoader } from '@/lib/loaders/userLoader'
 
 export const appRouter = createBrowserRouter([
   {
