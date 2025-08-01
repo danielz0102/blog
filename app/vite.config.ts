0f90e3d34a55ff5985165f1fd25d96038e32462a
danielz0102 <danyzglez@hotmail.com>
2025-08-01 14:01:04 -0600

    feat(app): add main layout

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,8 +3,12 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const isTestEnv = process.env.VITEST
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  // React Router is not designed to work with Vitest
+  // https://github.com/remix-run/react-router/discussions/12655
+  plugins: [tailwindcss(), !isTestEnv && reactRouter(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts']
