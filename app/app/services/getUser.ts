a5ede972c4ae399b5dcc54af85877d9645ccf7d1
danielz0102 <danyzglez@hotmail.com>
2025-07-31 18:29:03 -0600

    fix(app/services/getUser): return correct type

--- app/app/services/getUser.ts
+++ app/app/services/getUser.ts
@@ -1,7 +1,13 @@
+import type { User } from '~/types'
+import type { JwtPayload } from 'jwt-decode'
 import { jwtDecode } from 'jwt-decode'
+
+interface UserPayload extends JwtPayload, User {
+  admin: boolean
+}
 
 export function getUser() {
   const token = localStorage.getItem('token')
 
-  return token ? jwtDecode(token) : null
+  return token ? jwtDecode<UserPayload>(token) : null
 }
