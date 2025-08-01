a312bd160c03277ea9162e978635e7b9d502c671
danielz0102 <danyzglez@hotmail.com>
2025-08-01 00:47:36 -0600

    feat(app/services/comment): add delete service

--- app/app/services/comment.test.ts
+++ app/app/services/comment.test.ts
@@ -1,8 +1,10 @@
-import { test, expect, vi, afterEach } from 'vitest'
+import { describe, it, expect, vi, afterEach } from 'vitest'
 
 import { API_URL } from '~/config'
 
-import { comment } from './comment'
+import { comment, deleteComment } from './comment'
+
+const MOCK_TOKEN = 'mock-token'
 
 const mockFetch = vi.fn(() =>
   Promise.resolve({
@@ -12,71 +14,110 @@ const mockFetch = vi.fn(() =>
   })
 )
 
-const mockComment = {
-  postId: crypto.randomUUID(),
-  content: 'This is a test comment'
-}
-
-const MOCK_TOKEN = 'mock-token'
-
 vi.stubGlobal('fetch', mockFetch)
 
 afterEach(() => {
   localStorage.clear()
 })
 
-test('calls fetch correctly when creating a comment', async () => {
-  const { postId, content } = mockComment
-  localStorage.setItem('token', MOCK_TOKEN)
+describe('comment', () => {
+  const mockComment = {
+    postId: crypto.randomUUID(),
+    content: 'This is a test comment'
+  }
 
-  await comment(postId, content)
+  it('calls fetch correctly when creating a comment', async () => {
+    const { postId, content } = mockComment
+    localStorage.setItem('token', MOCK_TOKEN)
 
-  expect(mockFetch).toHaveBeenCalledWith(`${API_URL}/comments`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${MOCK_TOKEN}`
-    },
-    body: JSON.stringify(mockComment)
-  })
-})
+    await comment(postId, content)
 
-test('calls fetch correctly when updating a comment', async () => {
-  const { postId, content } = mockComment
-  localStorage.setItem('token', MOCK_TOKEN)
-
-  await comment(postId, content, true)
-
-  expect(mockFetch).toHaveBeenCalledWith(`${API_URL}/comments/${postId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${MOCK_TOKEN}`
-    },
-    body: JSON.stringify({ content })
-  })
-})
-
-test('throws an error if there is no token', async () => {
-  const { postId, content } = mockComment
-
-  const result = comment(postId, content)
-
-  await expect(result).rejects.toThrow()
-})
-
-test('throws an error if response is not ok', async () => {
-  const { postId, content } = mockComment
-  localStorage.setItem('token', MOCK_TOKEN)
-  mockFetch.mockImplementationOnce(() =>
-    Promise.resolve({
-      ok: false,
-      status: 500,
-      json: () => Promise.resolve({ error: 'Internal Server Error' })
+    expect(mockFetch).toHaveBeenCalledWith(`${API_URL}/comments`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${MOCK_TOKEN}`
+      },
+      body: JSON.stringify(mockComment)
     })
-  )
+  })
 
-  const result = comment(postId, content)
+  it('calls fetch correctly when updating a comment', async () => {
+    const { postId, content } = mockComment
+    localStorage.setItem('token', MOCK_TOKEN)
 
-  await expect(result).rejects.toThrow('Internal Server Error')
+    await comment(postId, content, true)
+
+    expect(mockFetch).toHaveBeenCalledWith(`${API_URL}/comments/${postId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${MOCK_TOKEN}`
+      },
+      body: JSON.stringify({ content })
+    })
+  })
+
+  it('throws an error if there is no token', async () => {
+    const { postId, content } = mockComment
+
+    const result = comment(postId, content)
+
+    await expect(result).rejects.toThrow()
+  })
+
+  it('throws an error if response is not ok', async () => {
+    const { postId, content } = mockComment
+    localStorage.setItem('token', MOCK_TOKEN)
+    mockFetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: false,
+        status: 500,
+        json: () => Promise.resolve({ error: 'Internal Server Error' })
+      })
+    )
+
+    const result = comment(postId, content)
+
+    await expect(result).rejects.toThrow('Internal Server Error')
+  })
+})
+
+describe('deleteComment', () => {
+  const commentId = crypto.randomUUID()
+
+  it('calls fetch correctly', async () => {
+    localStorage.setItem('token', MOCK_TOKEN)
+
+    await deleteComment(commentId)
+
+    expect(mockFetch).toHaveBeenCalledWith(`${API_URL}/comments/${commentId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${MOCK_TOKEN}`
+      }
+    })
+  })
+
+  it('throws an error if there is no token', async () => {
+    const result = deleteComment(commentId)
+
+    await expect(result).rejects.toThrow()
+  })
+
+  it('throws an error if response is not ok', async () => {
+    localStorage.setItem('token', MOCK_TOKEN)
+    mockFetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: false,
+        status: 500,
+        json: () => Promise.resolve({ error: 'Internal Server Error' })
+      })
+    )
+
+    const result = deleteComment(commentId)
+
+    await expect(result).rejects.toThrow('Internal Server Error')
+  })
 })
