a7a1c3e47c5949eaa131a1c6910fd2e11a4a9cd1
danielz0102 <danyzglez@hotmail.com>
2025-08-01 18:17:00 -0600

    feat(app): add Dialog component

--- app/app/components/Dialog/index.tsx
+++ app/app/components/Dialog/index.tsx
@@ -1,3 +1,27 @@
-export function Dialog({ children }: { children: React.ReactNode }) {
-  return <dialog>{children}</dialog>
+import { useRef, useEffect } from 'react'
+
+type DialogProps = {
+  children: React.ReactNode
+  open?: boolean
+}
+
+export function Dialog({ children, open = false }: DialogProps) {
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current?.showModal()
+    } else {
+      dialogRef.current?.close()
+    }
+  }, [open])
+
+  return (
+    <dialog ref={dialogRef}>
+      <button aria-label="Close" onClick={() => dialogRef.current?.close()}>
+        x
+      </button>
+      {children}
+    </dialog>
+  )
 }
