085a57168c0a905380c488bbce81f836922dd395
danielz0102 <danyzglez@hotmail.com>
2025-08-02 17:55:22 -0600

    feat(app/home): render most recent posts
    getPosts service was split into 2 functions to improve type handling

--- app/app/routes/home.tsx
+++ app/app/routes/home.tsx
@@ -1,4 +1,7 @@
 import type { Route } from './+types/home'
+import { getRecentPosts } from '~/services/posts'
+
+import { Link } from 'react-router'
 
 export function meta(): Route.MetaDescriptors {
   return [
@@ -7,6 +10,22 @@ export function meta(): Route.MetaDescriptors {
   ]
 }
 
-export default function Home() {
-  return <h1>Hello World</h1>
+export async function clientLoader() {
+  return getRecentPosts()
+}
+
+export default function Home({ loaderData: posts }: Route.ComponentProps) {
+  return (
+    <main>
+      <h1>Welcome to My Blog</h1>
+      <section>
+        {posts.map(({ id, title, createdAt }) => (
+          <article key={id}>
+            <h2><Link to={`/posts/${id}`}>{title}</Link></h2>
+            <time dateTime={createdAt}>{createdAt}</time>
+          </article>
+        ))}
+      </section>
+    </main>
+  )
 }
