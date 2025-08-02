0bd9958026cad2427f8a548dbd86916a7197016d
danielz0102 <danyzglez@hotmail.com>
2025-08-01 18:17:00 -0600

    feat(app): add Dialog component

--- app/app/components/Dialog/Dialog.test.tsx
+++ app/app/components/Dialog/Dialog.test.tsx
@@ -1,10 +1,37 @@
 import { test, expect } from 'vitest'
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Dialog } from '.'
 
-test('renders children insidea a dialog', () => {
+// JSDOM does not support the dialog element
+HTMLDialogElement.prototype.showModal = function () {
+  this.open = true
+}
+
+HTMLDialogElement.prototype.close = function () {
+  this.open = false
+}
+
+test('is hidden by default', () => {
   const { queryByText } = render(<Dialog>Test Dialog</Dialog>)
 
-  expect(queryByText('Test Dialog')).toBeInTheDocument()
+  expect(queryByText('Test Dialog')).not.toBeVisible()
+})
+
+test('renders children within a dialog when is open', () => {
+  const { queryByText } = render(<Dialog open>Test Dialog</Dialog>)
+
+  expect(queryByText('Test Dialog')).toBeVisible()
+})
+
+test('has a button to close', async () => {
+  const user = userEvent.setup()
+  const { getByRole } = render(<Dialog open>Test Dialog</Dialog>)
+  const dialog = getByRole('dialog')
+  const button = getByRole('button', { name: /close/i })
+
+  await user.click(button)
+
+  expect(dialog).not.toBeVisible()
 })
