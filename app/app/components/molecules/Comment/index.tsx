cb577fd608fe6e671a79fa9276869f4418fa918d
danielz0102 <danyzglez@hotmail.com>
2025-08-04 15:11:12 -0600

    refactor(app): extract CommentList component

--- app/app/components/molecules/Comment/index.tsx
+++ app/app/components/molecules/Comment/index.tsx
@@ -1,6 +1,6 @@
 import type { Comment } from '~/types'
 
-export function Comment({ comment }: { comment: Comment }) {
+export default function Comment({ comment }: { comment: Comment }) {
   return (
     <article key={comment.id}>
       <h3>{comment.user.username}</h3>
