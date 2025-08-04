adbba41855e098318e6cdc83b57ab5b5f02e7a09
danielz0102 <danyzglez@hotmail.com>
2025-08-04 14:09:32 -0600

    feat(app): add page detail page

--- app/app/routes.ts
+++ app/app/routes.ts
@@ -9,6 +9,7 @@ export default [
   layout('./layouts/MainLayout.tsx', [
     index('routes/home.tsx'),
     route('auth/:action', 'routes/auth.ts'),
-    route('logout', 'routes/logout.ts')
+    route('logout', 'routes/logout.ts'),
+    route('posts/:id', 'routes/post.tsx')
   ])
 ] satisfies RouteConfig
