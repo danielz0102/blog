7c789044ee40f2d6644a53c51b74467a51f7f183
danielz0102 <danyzglez@hotmail.com>
2025-08-07 18:09:31 -0600

    feat(app): style dialog component

--- app/app/components/molecules/Dialog/index.tsx
+++ app/app/components/molecules/Dialog/index.tsx
@@ -1,3 +1,5 @@
+import CloseIcon from '~/components/atoms/CloseIcon'
+
 export interface DialogProps {
   children: React.ReactNode
   ref: React.RefObject<HTMLDialogElement | null>
@@ -5,9 +7,16 @@ export interface DialogProps {
 
 export function Dialog({ children, ref }: DialogProps) {
   return (
-    <dialog ref={ref}>
-      <button aria-label="Close" onClick={() => ref?.current?.close()}>
-        x
+    <dialog
+      ref={ref}
+      className="absolute inset-1/2 -translate-1/2 rounded-lg p-4"
+    >
+      <button
+        aria-label="Close"
+        onClick={() => ref?.current?.close()}
+        className="absolute top-2 right-2 cursor-pointer"
+      >
+        <CloseIcon />
       </button>
       {children}
     </dialog>
--- app/app/routes/home.tsx
+++ app/app/routes/home.tsx
@@ -17,7 +17,7 @@ export async function clientLoader() {
 export default function Home({ loaderData: posts }: Route.ComponentProps) {
   return (
     <main className="mx-auto flex max-w-2xl flex-1 flex-col gap-4 px-4 py-4">
-      <h1 className="border-b-w-full border-b-1 border-b-zinc-400 pb-1 text-3xl font-bold">
+      <h1 className="border-b-w-full border-b border-b-zinc-400 pb-1 text-3xl font-bold">
         Recent Posts
       </h1>
       <section className="flex flex-col gap-4">
