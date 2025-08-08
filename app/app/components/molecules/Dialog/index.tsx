b4870608be3685ec616cb0dd6747e424ba433fc0
danielz0102 <danyzglez@hotmail.com>
2025-08-07 19:20:35 -0600

    feat(app/Dialog): add fade transition

--- app/app/components/molecules/Dialog/index.tsx
+++ app/app/components/molecules/Dialog/index.tsx
@@ -9,7 +9,7 @@ export function Dialog({ children, ref }: DialogProps) {
   return (
     <dialog
       ref={ref}
-      className="absolute inset-1/2 -translate-1/2 rounded-lg p-8 md:min-w-2xl"
+      className="absolute inset-1/2 -translate-1/2 rounded-lg p-8 transition-[opacity,display] transition-discrete backdrop:backdrop-blur-xs md:min-w-2xl starting:opacity-0 [&:not([open])]:opacity-0"
     >
       <button
         aria-label="Close"
