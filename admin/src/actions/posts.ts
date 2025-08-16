f4765ec56b7181dd7202611c52d8a9bf5c953283
danielz0102 <danyzglez@hotmail.com>
2025-08-16 00:50:17 -0600

    refactor(admin): define actions outside export to remove unnecessary indentation

--- admin/src/actions/posts.ts
+++ admin/src/actions/posts.ts
@@ -4,42 +4,44 @@ import { API_URL } from 'astro:env/server'
 import { ADMIN_TOKEN_COOKIE } from '@/consts'
 import type { Post } from '@/env'
 
-export const posts = {
-  getAll: defineAction({
-    handler: async (_, ctx): Promise<Post[]> => {
-      const tokenCookie = ctx.cookies.get(ADMIN_TOKEN_COOKIE)
+const getAll = defineAction({
+  handler: async (_, ctx): Promise<Post[]> => {
+    const tokenCookie = ctx.cookies.get(ADMIN_TOKEN_COOKIE)
 
-      if (!tokenCookie) {
-        throw new ActionError({
-          code: 'UNAUTHORIZED',
-          message: 'Authentication failed',
-        })
-      }
-
-      const response = await fetch(`${API_URL}/posts/all`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${tokenCookie.value}`,
-        },
+    if (!tokenCookie) {
+      throw new ActionError({
+        code: 'UNAUTHORIZED',
+        message: 'Authentication failed',
       })
+    }
 
-      if (!response.ok) {
-        throw new ActionError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'Posts could not be retrieved',
-        })
-      }
+    const response = await fetch(`${API_URL}/posts/all`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${tokenCookie.value}`,
+      },
+    })
 
-      const data: Post[] = await response.json()
-      return data.map((post) => ({
-        ...post,
-        createdAt: new Date(post.createdAt).toLocaleDateString('en-US', {
-          day: 'numeric',
-          month: 'long',
-          year: 'numeric',
-        }),
-      }))
-    },
-  }),
+    if (!response.ok) {
+      throw new ActionError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Posts could not be retrieved',
+      })
+    }
+
+    const data: Post[] = await response.json()
+    return data.map((post) => ({
+      ...post,
+      createdAt: new Date(post.createdAt).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      }),
+    }))
+  },
+})
+
+export const posts = {
+  getAll,
 }
--- admin/src/actions/user.ts
+++ admin/src/actions/user.ts
@@ -7,56 +7,60 @@ import { z } from 'astro:schema'
 import { jwtDecode } from 'jwt-decode'
 import { ADMIN_TOKEN_COOKIE } from '@/consts'
 
+const login = defineAction({
+  input: z.object({ username: z.string(), password: z.string() }),
+  accept: 'form',
+  handler: async (input, ctx) => {
+    const response = await fetch(`${API_URL}/users/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
+    })
+
+    if (!response.ok) {
+      if (response.status === 401 || response.status === 400) {
+        throw new ActionError({
+          code: 'UNAUTHORIZED',
+          message: 'Username or password is not valid',
+        })
+      }
+
+      throw new ActionError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Oops, something went wrong. Try it again later.',
+      })
+    }
+
+    const data: { token: string } = await response.json()
+    const decoded = jwtDecode<UserPayload>(data.token)
+
+    if (!decoded.admin) {
+      throw new ActionError({
+        code: 'FORBIDDEN',
+        message: 'You do not have permission to access this resource',
+      })
+    }
+
+    const SEVEN_DAYS = 60 * 60 * 1000 * 24 * 7
+
+    ctx.cookies.set(ADMIN_TOKEN_COOKIE, data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      expires: new Date(Date.now() + SEVEN_DAYS),
+    })
+  },
+})
+
+const logout = defineAction({
+  handler: async (_, ctx) => {
+    ctx.cookies.delete(ADMIN_TOKEN_COOKIE, { path: '/' })
+  },
+})
+
 export const user = {
-  login: defineAction({
-    input: z.object({ username: z.string(), password: z.string() }),
-    accept: 'form',
-    handler: async (input, ctx) => {
-      const response = await fetch(`${API_URL}/users/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(input),
-      })
-
-      if (!response.ok) {
-        if (response.status === 401 || response.status === 400) {
-          throw new ActionError({
-            code: 'UNAUTHORIZED',
-            message: 'Username or password is not valid',
-          })
-        }
-
-        throw new ActionError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'Oops, something went wrong. Try it again later.',
-        })
-      }
-
-      const data: { token: string } = await response.json()
-      const decoded = jwtDecode<UserPayload>(data.token)
-
-      if (!decoded.admin) {
-        throw new ActionError({
-          code: 'FORBIDDEN',
-          message: 'You do not have permission to access this resource',
-        })
-      }
-
-      const SEVEN_DAYS = 60 * 60 * 1000 * 24 * 7
-
-      ctx.cookies.set(ADMIN_TOKEN_COOKIE, data.token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        expires: new Date(Date.now() + SEVEN_DAYS),
-      })
-    },
-  }),
-  logout: defineAction({
-    handler: async (_, ctx) => {
-      ctx.cookies.delete(ADMIN_TOKEN_COOKIE, { path: '/' })
-    },
-  }),
+  login,
+  logout,
 }
