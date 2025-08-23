7ce74b246671ca0b1ade8d1ea9d6a73637ca61a5
danielz0102 <danyzglez@hotmail.com>
2025-08-23 00:46:16 -0600

    build(admin): add netlify adapter

--- admin/astro.config.mjs
+++ admin/astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig, envField } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
+import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   output: 'server',
   integrations: [react()],
+  adapter: netlify(),
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
   env: {
     schema: {
       API_URL: envField.string({ context: 'server', access: 'public' }),
       TOKEN_SECRET: envField.string({ context: 'server', access: 'secret' }),
-      TINYMCE_API_KEY: envField.string({ context: 'client', access: 'public' }),
-    },
-  },
+      TINYMCE_API_KEY: envField.string({ context: 'client', access: 'public' })
+    }
+  }
 })
